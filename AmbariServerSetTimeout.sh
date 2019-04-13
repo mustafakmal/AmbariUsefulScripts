5f34bf6d426a16f43fc62e7c126c6ab4ceb0db37
Mustafa Akmal <mustafakmal95@gmail.com>
2019-04-13 16:09:37 +0500

    Update AmbariServerSetTimeout.sh

--- AmbariServerSetTimeout.sh
+++ AmbariServerSetTimeout.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+ambari_properties_file="/etc/ambari-server/conf/ambari.properties"
 
-if grep -q server.startup.web.timeout "/etc/ambari-server/conf/ambari.properties"; then
-  # SomeString was found.Delete The Line
-  sed '/server.startup.web.timeout/d' /etc/ambari-server/conf/ambari.properties
-  /etc/ambari-server/conf/ambari.properties >> server.startup.web.timeout=$1
-  
-  else
-  /etc/ambari-server/conf/ambari.properties >> server.startup.web.timeout=$1
-  
+if grep -q server.startup.web.timeout $ambari_properties_file; then
+    # SomeString was found.Delete The Line
+    sed '/server.startup.web.timeout/d' $ambari_properties_file
+    $ambari_properties_file >> server.startup.web.timeout=$1
+    
+else
+    $ambari_properties_file >> server.startup.web.timeout=$1
+    
 fi
